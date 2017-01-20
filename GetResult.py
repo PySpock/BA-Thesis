8d6529a2dd05c3476f31d93dbd67f22dc6b622d5
PySpock <meister90klasse@gmail.com>
2017-01-20 05:08:41 +0100

    TempFix: volume fraction

--- GetResult.py
+++ GetResult.py
@@ -1,5 +1,7 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
+import AnalyticModels as am
 
 def readFile(name):
 	try:
@@ -31,11 +33,35 @@ def separateKeysVals(line_list, delimiter):
 # Path data to TXT-file with simulation results
 # Specify path and name ("hardcoded variables")
 
-filepath = 'C:\\Users\\Jannik\\Desktop\\5 Sphere varVolFrac'
-resultfile = 'Results.txt'
+filepath = 'C:\\Users\\Jannik\\Desktop\\NewData'
+resultfile = 'RandSphere.txt'
 delimiter = '-----'
 
-lines = readFile(resultfile)
-keys, vals = separateKeysVals(lines, delimiter)
+if os.path.isdir(filepath):
+	os.chdir(filepath)
+	lines = readFile(resultfile)
+	keys, vals = separateKeysVals(lines, delimiter)
+else:
+	print('Path does not exist: ' + filepath)
 
 # Code, e.g. plotting
+
+print(keys)
+print(vals)
+
+vF = np.linspace(0.01,0.25,200)
+ki = 1.00
+km = 0.01
+
+v1corr = [0.25 * vFrac for vFrac in vals[1]]
+
+mx = am.maxwell(ki,km,vF)
+cg = am.ChiewGland(ki,km,vF)
+rl = am.rayleigh(ki,km,vF)
+
+plt.plot(v1corr, vals[0], 'ro', label='kEff from FEM/numerical')
+plt.plot(vF, mx, 'g-', label='Maxwell')
+plt.plot(vF, cg, 'b-', label='ChiewGland')
+plt.plot(vF, rl, 'y-', label='Rayleigh')
+plt.legend(loc=2)
+plt.show()
