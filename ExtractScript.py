d7bf5a8ff1b80e49b82e59b85a09e58fd1335af1
PySpock <meister90klasse@gmail.com>
2017-01-20 04:31:00 +0100

    Improved exception handling

--- ExtractScript.py
+++ ExtractScript.py
@@ -3,9 +3,9 @@ import datetime as dt
 
 
 def escCharCleanup(strList):
-	# Returns eine String-List ohne /n-Escape-Char, wenn der an letzer Stelle eines Elements steht
-	noEscData = [entry[:-1] if entry[-1:] == '\n' else entry[:] for entry in strList]
-	return noEscData
+	# Returns die strList /n-Escape-Char, wenn der an erst . od. letz. Stelle eines Elements steht
+	cleanData = [entry.strip() for entry in strList]
+	return cleanData
 
 def clearHeader(strList, delimiterLine):
 	# Returns Liste ohne unwichtige Header-Elemente bis einschließlich delimiter-Zeile
@@ -21,8 +21,11 @@ def splitData(strList, splitChar):
 	keyList = []
 	valList = []
 	for element in strList:
-		keyList.append(element.split(splitChar)[0])
-		valList.append(float(element.split(splitChar)[1]))
+		try:
+			keyList.append(element.split(splitChar)[0])
+			valList.append(float(element.split(splitChar)[1]))
+		except IndexError:
+			pass
 	return keyList, valList
 
 def getint(name):
@@ -52,8 +55,13 @@ def writeFile(name, keys, vals):
 	height = len(vals)
 
 	for valkey in range(width):
+		first_element = True
 		for valrun in range(height):
-			file.write(str(vals[valrun][valkey]) + ',')
+			if first_element:
+				file.write(str(vals[valrun][valkey]))
+				first_element = False
+			else:
+				file.write(',' + str(vals[valrun][valkey]))
 		file.write('\n')
 	file.close()
 
@@ -67,46 +75,57 @@ print(' ')
 print(' ')
 
 while True:
-	try:
-		print('Please input the directory in quotes and double slashes, where the flexPDE summaries are saved.')
+	while True:
+		print('Please input the directory with double slashes, where the flexPDE summaries are saved.')
 		path = input('Input path: ')
 		if os.path.isdir(path):
 			print('Success, directory exists')
 			os.chdir(path)
 			break
-	except:
-		pass
-	print('Error: No directory found')
-
-print('Listing txt-content of set working directory ', os.getcwd(), ' ...')
-print(' ')
-
-exTXT = False
-for entry in os.listdir(os.getcwd()):
-	if entry[-4:] == '.txt':
-		print(entry)
-		exTXT = True
-if exTXT == False:
-	print('No existing TXT-Files in this directory!') 
+		print('Error: No such directory found')
 
 
-data_files = getFiles('Sim_Info')
-mf_keys = []
-mf_vals = []
-for filename in data_files:
-	file = open(filename, 'r')
-	# Inhalt des Files wird als Liste eingelesen, Element von content entspricht einer Zeile
-	sf_content = list(file)
-	sf_cleanData = clearHeader(escCharCleanup(sf_content), ' -----')
-	keys, vals = splitData(sf_cleanData, '=')
-	mf_keys = keys
-	mf_vals.append(vals)
+	print('Listing txt-content of set working directory ', os.getcwd(), ' ...')
+	print(' ')
 
-print(' ')
-print(' ')
-print('Please input the name for the results file: ')
-name = input()
-print('Creating result file ' + str(name) + '.txt ...')
-writeFile(name, mf_keys, mf_vals)
-print('Done, press Enter to exit')
-dummy = input()
+	exTXT = False
+	for entry in os.listdir(os.getcwd()):
+		if entry[-4:] == '.txt':
+			print(entry)
+			exTXT = True
+	if exTXT == False:
+		print('No existing TXT-Files in this directory!') 
+
+
+	data_files = getFiles('Sim_Info_')
+	mf_keys = []
+	mf_vals = []
+	for filename in data_files:
+		file = open(filename, 'r')
+		# Inhalt des Files wird als Liste eingelesen, Element von content entspricht einer Zeile
+		sf_content = list(file)
+		sf_cleanData = clearHeader(escCharCleanup(sf_content), '-----')
+		keys, vals = splitData(sf_cleanData, '=')
+		mf_keys = keys
+		mf_vals.append(vals)
+
+	print(' ')
+	print(' ')
+	print('Please input the name for the results file: ')
+	name = input()
+	print('Creating result file ' + str(name) + '.txt ...')
+	writeFile(name, mf_keys, mf_vals)
+	print('Done!')
+
+	cont = True
+	while True:
+		print('Continue with another task [C] or exit [X]?')
+		action = input()
+		if action == 'C' or action == 'c':
+			break
+		elif action == 'X' or action == 'x':
+			cont = False
+			break
+		print('Invalid input!')
+	if cont == False:
+		break
--- GetResult.py
+++ GetResult.py
@@ -2,27 +2,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def readFile(name):
-	file = open(name, 'r')
-	line_list_raw = list(file)
-	file.close()
-	line_list = [line.strip() for line in line_list_raw]
+	try:
+		file = open(name, 'r')
+	except IOError:
+		print('File ' + name + ' not found or inaccessible')
+		line_list = []
+		pass
+	else:
+		line_list_raw = list(file)
+		file.close()
+		line_list = [line.strip() for line in line_list_raw]
 	return line_list
 
-def separateKeysVals(line_list,delimiter):
-	split_index = line_list.index(delimiter)
-	key_lines = line_list[:split_index]
-	val_lines = line_list[split_index + 1:]
-	val_lines_clean = []
-	for stritem in val_lines:
-		cache = stritem.split(',')
-		val_lines_clean.append([float(value) for value in cache[:-1]])
+def separateKeysVals(line_list, delimiter):
+	try:
+		split_index = line_list.index(delimiter)
+	except ValueError:
+		return ('Empty', 'Empty')
+	else:
+		key_lines = line_list[:split_index]
+		val_lines = line_list[split_index + 1:]
+		val_lines_clean = []
+		for stritem in val_lines:
+			cache = stritem.split(',')
+			val_lines_clean.append([float(value) for value in cache[:-1]])
 	return key_lines, val_lines_clean
 
+test = ['hans','wurst','-----','1,2,3,4,5,','6,7,8,9,10,']
 
 resultfile = 'Results.txt'
 delimiter = '-----'
 lines = readFile(resultfile)
-keys, vals = separateKeysVals(test, delimiter)
+keys, vals = separateKeysVals(lines, delimiter)
+
+print(keys)
+print(vals)
 
 # Man kann jetzt mit den keys und values sauber weiterarbeiten und z.b. Plotten
 
