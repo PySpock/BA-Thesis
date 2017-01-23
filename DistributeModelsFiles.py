7107a3eee236dc219382fc34e2b81e0a8b359c53
PySpock <meister90klasse@gmail.com>
2017-01-23 23:46:51 +0100

    Minor fixes

--- DistributeModelsFiles.py
+++ DistributeModelsFiles.py
@@ -1,46 +1,49 @@
 import os
 import shutil
 
-while True:
-	print('AnalyticModel Distribution Script')
-	print(' ')
-	print(' ')
-	print('Directory is ' + os.getcwd())
-	print('Listing content: ')
-	for item in os.listdir(os.getcwd()):
-		print(item)
+# Script tries to locate the AnalyticalModels.py file in the current working directory
+# It then copies the file to all sub-subfolders of the specified subfolder 
+# here: '\\Graph Generation' in the current working directory
 
-	print('Trying to locate AnalyticModels.py in current working directory ...')
-	if 'AnalyticModels.py' in os.listdir(os.getcwd()):
-		print('File found!')
-		am_path = os.getcwd() + '\\AnalyticModels.py'
-	else:
-		print('Fatal error: File AnalyticModels.py not found!')
-		print('Press enter to exit')
-		dummy = input()
-		break
 
-	print('Trying to open folder Graph Generation in current working directory ...')
-	subfolder = '\\Graph Generation'
-	try:
-		fold_content = os.listdir(os.getcwd() + subfolder)
-		print(fold_content)
-	except OSError:
-		print('Fatal Error: Could not open ' + os.getcwd() + subfolder)
-		print('Press enter to exit')
-		dummy = input()
-		break
-	else:
-		print('Success!')
-		folders_path = [os.getcwd() + subfolder + '\\' + item
-						for item in fold_content 
-						if os.path.isdir(os.getcwd() + subfolder + '\\' + item)]
-		print(folders_path)
+print('AnalyticModel Distribution Script')
+print(' ')
+print(' ')
+print('Directory is ' + os.getcwd())
+print('Listing content: ')
+for item in os.listdir(os.getcwd()):
+	print(item)
 
-	for dest in folders_path:
-		shutil.copy(am_path, dest)
-		print('Copied AnalyticModels.py successfully to ' + dest)
-	print('Succesfully copied to all subfolders')
-	print('Press Enter to exit script')
+print('Trying to locate AnalyticModels.py in current working directory ...')
+if 'AnalyticModels.py' in os.listdir(os.getcwd()):
+	print('File found!')
+	am_path = os.getcwd() + '\\AnalyticModels.py'
+else:
+	print('Fatal error: File AnalyticModels.py not found!')
+	print('Press enter to exit')
 	dummy = input()
 	break
+
+print('Trying to open folder Graph Generation in current working directory ...')
+subfolder = '\\Graph Generation'
+try:
+	fold_content = os.listdir(os.getcwd() + subfolder)
+	print(fold_content)
+except OSError:
+	print('Fatal Error: Could not open ' + os.getcwd() + subfolder)
+	print('Press enter to exit')
+	dummy = input()
+	break
+else:
+	print('Success!')
+	folders_path = [os.getcwd() + subfolder + '\\' + item
+					for item in fold_content 
+					if os.path.isdir(os.getcwd() + subfolder + '\\' + item)]
+	print(folders_path)
+
+for dest in folders_path:
+	shutil.copy(am_path, dest)
+	print('Copied AnalyticModels.py successfully to ' + dest)
+print('Succesfully copied to all subfolders')
+print('Press Enter to exit script')
+dummy = input()
