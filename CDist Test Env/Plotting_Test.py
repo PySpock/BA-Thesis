fc1f503f3ee8cf60a276d11536dce4de7df2e2d3
PySpock <meister90klasse@gmail.com>
2017-03-10 13:01:58 +0100

    New Plotstyles

--- CDist Test Env/Plotting_Test.py
+++ CDist Test Env/Plotting_Test.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
 import os
 import itertools
@@ -57,7 +58,7 @@ def extract_from_flextbl(filename, separators=['X', 'Y', 'DATA']):
 	return data_list
 
 
-os.chdir(r'C:\Users\stebanij\Desktop\N=5 sym_spaced Spheres - Kopie')
+#os.chdir(r'C:\Users\stebanij\Desktop\N=5 sym_spaced Spheres - Kopie')
 
 file = '5_Sphere_varVolFrac_01.tbl'
 
@@ -85,7 +86,7 @@ print('YYY: ', yc[k])
 print('ZZZ: ', z[k])
 """
 
-cont = np.linspace(10, 50, 30)
+cont = np.linspace(10, 50, 400)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -94,12 +95,23 @@ rstride = 1
 cstride = 1
 
 ax.set_xlabel('X')
-ax.set_ylabel('Y')
+ax.set_ylabel('Y', rotation='horizontal')
+ax.set_yticks([-0.1, 0, 0.1])
 #ax.set_zlabel('TEMP')
-ax.set_aspect('equal')
+ax.set_aspect(aspect=1)
 
-ax.contour(xc, yc, z, levels=cont)
+#ct = ax.contour(xc, yc, z, levels=cont, antialiased=True)
+ctf = ax.contourf(xc, yc, z, levels=cont)
 
+divider = make_axes_locatable(ax)
+cax  = divider.append_axes('top', size='10%', pad=0.3)
+
+
+cbar = plt.colorbar(ctf, cax=cax, orientation='horizontal')
+cbar.ax.xaxis.set_ticks_position('top')
+cbar.ax.set_ylabel('Temp', labelpad=30, rotation='horizontal')
+cbar_ticks = np.arange(10, 60, 5)
+cbar.set_ticks(cbar_ticks)
 
 plt.show()
 
